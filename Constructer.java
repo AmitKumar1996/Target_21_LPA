fece9363778fad6a4d62c3c033cb425ab660d30d
AmitKumar1996 <kumaramit1996.1@gmail.com>
2024-03-22 19:50:57 +0530

    check output

--- Constructer.java
+++ Constructer.java
@@ -16,7 +16,7 @@ class Persion extends Student{
      * @param marks
      */
     Persion(int rollNo, int marks){
-          super(name, age)
+          super(name, age);
           this.rollNo=rollNo;
           this.marks=marks;
     }
@@ -29,5 +29,14 @@ class Persion extends Student{
 public class Constructer {
  public static void main(String[] args){
     System.out.println("Jay Shri Ram");
+    Student s=new Student("Amit", 29);
+    System.out.println(s.hashCode());
+    System.out.println(s.name);
+    System.out.println(s.age);
+
+    Persion p=new Persion(29,101);
+    System.out.println(p.hashCode());
+    System.out.println(p.age);
+    System.out.println(p.rollNo);
  }   
 }
