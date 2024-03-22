8bd04029193cb2a9687527e41256262f07f4ea5f
AmitKumar1996 <kumaramit1996.1@gmail.com>
2024-03-22 19:42:01 +0530

    making person class and inherited Student class

--- Constructer.java
+++ Constructer.java
@@ -1,13 +1,27 @@
 class Student{
-    String name;
-    int age;
+    static String name;
+   static int age;
     Student(String name, int age){
         this.name=name;
         this.age=age;
     }
 }
 
+class Persion extends Student{
+    int rollNo;
+    int marks;
 
+    /**
+     * @param rollNo
+     * @param marks
+     */
+    Persion(int rollNo, int marks){
+          super(name, age)
+          this.rollNo=rollNo;
+          this.marks=marks;
+    }
+
+}
 
 
 
