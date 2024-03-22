da180ac1087d24ec46ccda9391a491fe61b8f47d
AmitKumar1996 <kumaramit1996.1@gmail.com>
2024-03-22 19:34:01 +0530

    Student class

--- Constructer.java
+++ Constructer.java
@@ -1,3 +1,17 @@
+class Student{
+    String name;
+    int age;
+    Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+}
+
+
+
+
+
+
 public class Constructer {
  public static void main(String[] args){
     System.out.println("Jay Shri Ram");
